3ff66a8dfe1152d495044b6d4e0778189bbd894c
grandaz <20200945@aloe.ulima.edu.pe>
2023-10-08 15:46:41 -0500

    pedidos

--- src/components/Cards/CardPedido.tsx
+++ src/components/Cards/CardPedido.tsx
@@ -1,54 +1,61 @@
 import {
-    Card,
-    CardHeader,
-    CardBody,
-    CardFooter,
-    Typography,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Typography,
 } from "@material-tailwind/react";
 import GreenButton from "../Inputs/GreenButton";
-import RatingStars from "../Rating/RatingStars";
-import Logo from '../../assets/icon1.png'
 
-function CardPedido() {
-    return (
-        <Card className="w-96">
-            
-            <CardHeader shadow={false} floated={false} className="h-26">
-            <div className="flex items-center space-x-4">
-                    <img className="w-10 h-10 rounded-full" src={Logo} alt=""></img>
-                    <div className="font-medium dark:text-white">
-                        <div>Jese Leos</div>
-                        <RatingStars></RatingStars>
-                    </div>
-                </div>
-                
+interface CardPedidoProps {
+  logoEmpresa: string
+  empresa: string
+  fecha: string
+  producto: string
+  detalle: string
+  precio: number
+}
 
-                
-            </CardHeader>
-            <CardBody>
-                <div className="mb-2 flex items-center justify-between">
-                    <Typography color="blue-gray" className="font-medium">
-                        Apple AirPods
-                    </Typography>
-                    <Typography color="blue-gray" className="font-medium">
-                        $95.00
-                    </Typography>
-                </div>
-                <Typography
-                    variant="small"
-                    color="gray"
-                    className="font-normal opacity-75"
-                >
-                    With plenty of talk and listen time, voice-activated Siri access, and
-                    an available wireless charging case.
-                </Typography>
-            </CardBody>
-            <CardFooter className="pt-0">
-                
-                <GreenButton type="button" label="Add to Cart"></GreenButton>
-            </CardFooter>
-        </Card>
-    );
+function CardPedido(props: CardPedidoProps) {
+  return (
+    <Card className="w-96">
+      <CardHeader shadow={false} floated={false} className="h-26">
+        <div className="flex items-center space-x-4">
+          <img className="w-10 h-10 rounded-full" src={props.logoEmpresa} alt=""></img>
+          <div className="font-medium dark:text-white">
+            <div>{props.empresa}</div>
+            <Typography
+              variant="small"
+              color="gray"
+              className="font-normal opacity-75"
+            >
+              Publicado: {props.fecha}
+            </Typography>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody>
+        <div className="mb-2 flex items-center justify-between">
+          <Typography color="blue-gray" className="font-medium">
+            {props.producto}
+          </Typography>
+          <Typography color="blue-gray" className="font-medium">
+            {'$' + props.precio}
+          </Typography>
+        </div>
+        <Typography
+          variant="small"
+          color="gray"
+          className="font-normal opacity-75"
+        >
+          {props.detalle}
+        </Typography>
+      </CardBody>
+      <CardFooter className="pt-0">
+        <GreenButton type="button" label="Inscribirse"></GreenButton>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default CardPedido
--- src/components/NavBar/LandingNavBar.tsx
+++ src/components/NavBar/LandingNavBar.tsx
@@ -1,39 +1,83 @@
+import { Link, useLocation } from 'react-router-dom'
 import Logo from '../../assets/logo1_icon.png'
 import GreenButton from '../Inputs/GreenButton'
 
 export default function LandingNavBar() {
+
+    const location = useLocation();
+
     return (
         <>
-        <nav className="bg-white light:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 light:border-gray-600">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/inicio" className="flex items-center">
-                    <img src={Logo} className="h-8 mr-3" alt="Allpa Logo"></img>
-                </a>
-                <div className="flex md:order-2">
-                    <a href='/login'><GreenButton type='button' label='Iniciar sesión'></GreenButton></a>
+            <nav className="bg-white light:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 light:border-gray-600">
+                <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="/inicio" className="flex items-center">
+                        <img src={Logo} className="h-8 mr-3" alt="Allpa Logo"></img>
+                    </a>
+                    <div className="flex md:order-2">
+                        <a href='/login'><GreenButton type='button' label='Iniciar sesión'></GreenButton></a>
+                    </div>
+                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                        <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white">
+                            <li>
+                                <Link
+                                    to="/inicio"
+                                    className={`block py-2 pl-3 pr-4 ${location.pathname === '/inicio'
+                                        ? 'md:text-emerald-700'
+                                        : 'md:hover:text-emerald-700'
+                                        } md:p-0`}
+                                >
+                                    Inicio
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/pedidos"
+                                    className={`block py-2 pl-3 pr-4 ${location.pathname === '/pedidos'
+                                        ? 'md:text-emerald-700'
+                                        : 'md:hover:text-emerald-700'
+                                        } md:p-0`}
+                                >
+                                    Pedidos
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/productos"
+                                    className={`block py-2 pl-3 pr-4 ${location.pathname === '/productos'
+                                        ? 'md:text-emerald-700'
+                                        : 'md:hover:text-emerald-700'
+                                        } md:p-0`}
+                                >
+                                    Productos
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/nosotros"
+                                    className={`block py-2 pl-3 pr-4 ${location.pathname === '/nosotros'
+                                        ? 'md:text-emerald-700'
+                                        : 'md:hover:text-emerald-700'
+                                        } md:p-0`}
+                                >
+                                    Sobre nosotros
+                                </Link>
+                            </li>
+                            <li>
+                                <Link
+                                    to="/soporte"
+                                    className={`block py-2 pl-3 pr-4 ${location.pathname === '/soporte'
+                                        ? 'md:text-emerald-700'
+                                        : 'md:hover:text-emerald-700'
+                                        } md:p-0`}
+                                >
+                                    Soporte
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white light:bg-gray-800 md:light:bg-gray-900 light:border-gray-700">
-                        <li>
-                            <a href="/inicio" className="block py-2 pl-3 pr-4 text-white bg-emerald-700 rounded md:bg-transparent md:text-emerald-700 md:p-0 md:light:text-emerald-500" aria-current="page">Inicio</a>
-                        </li>
-                        <li>
-                            <a href="/pedidos" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 md:light:hover:text-emerald-500 light:text-white light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700">Pedidos</a>
-                        </li>
-                        <li>
-                            <a href="/productos" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 md:light:hover:text-emerald-500 light:text-white light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700">Productos</a>
-                        </li>
-                        <li>
-                            <a href="/nosotros" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 md:light:hover:text-emerald-500 light:text-white light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700">Sobre nosotros</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-emerald-700 md:p-0 md:light:hover:text-emerald-500 light:text-white light:hover:bg-gray-700 light:hover:text-white md:light:hover:bg-transparent light:border-gray-700">Soporte</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <div className="h-16"></div>
+            </nav>
+            <div className="h-16"></div>
         </>
     )
 }
