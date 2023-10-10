a9597bdc679095dc84a69773709928cb1b9c3241
Juan Diego Bernuy Rey <JD>
2023-10-10 11:14:45 -0500

    JD

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -11,7 +11,7 @@ export default function SignIn() {
       <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-                  Create and account
+                  Create an account
               </h1>
               <form className="space-y-4 md:space-y-6" action="/landing">
                   <div>
