e102ab37d791d2ecc564725693911e4437c9500d
JanoGomezL <132672653+JanoGomezL@users.noreply.github.com>
2023-10-09 19:21:32 -0500

    Add files via upload
    botones redirigen a otras pantallas

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,5 +1,6 @@
 import InputField from "../components/Inputs/InputField"
 import Logo from '../assets/icon1.png'
+import { Link } from "react-router-dom"
 
 export default function Login() {
     return (
@@ -34,9 +35,9 @@ export default function Login() {
                                 </div>
                                 <a href="#" className="text-sm font-medium text-primary-600 hover:underline">Forgot password?</a>
                             </div>
-                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center">Sign in</button>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center"><Link to="http://localhost:5173/Inicio">Sign in</Link></button>
                             <p className="text-sm font-light text-gray-500">
-                                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline">Sign up</a>
+                                Don't have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline"><Link to="http://localhost:5173/register">Sign up</Link></a>
                             </p>
                         </form>
                     </div>
