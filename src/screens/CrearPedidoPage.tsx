500f3724ff178ce22faeccf18204232171504e51
grandaz <20200945@aloe.ulima.edu.pe>
2023-10-12 10:09:06 -0500

    gg

--- src/screens/CrearPedidoPage.tsx
+++ src/screens/CrearPedidoPage.tsx
@@ -7,7 +7,11 @@ interface CrearPedidoPageProps {
 
 }
 
-class CrearPedidoPage extends Component<CrearPedidoPageProps> {
+interface CrearPedidoPageState {
+    listaProductosForm: number[];
+}
+
+class CrearPedidoPage extends Component<CrearPedidoPageProps, CrearPedidoPageState> {
 
     productos = [
         {
@@ -30,7 +34,24 @@ class CrearPedidoPage extends Component<CrearPedidoPageProps> {
 
     constructor(props: CrearPedidoPageProps) {
         super(props);
+
+        this.state = {
+            listaProductosForm: [this.productos[0].id],
+        }
     }
+
+    private agregarProducto() {
+        this.setState((prevState) => {
+            return { listaProductosForm: [...prevState.listaProductosForm, this.productos[0].id] };
+        })
+    }
+
+    private eliminarProducto = (index: number) => {
+        this.setState((prevState) => ({
+            listaProductosForm: prevState.listaProductosForm.filter((_, i) => i !== index),
+        }));
+    };
+
 
     render() {
         return (
@@ -41,34 +62,34 @@ class CrearPedidoPage extends Component<CrearPedidoPageProps> {
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Título</label>
                         <InputField type={'text'}></InputField>
                     </div>
-                    <div className="grid gap-6 mb-5 md:grid-cols-4">
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Producto</label>
-                            <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-primary-600 focus:border-primary-600">
-                                {
-                                    this.productos.map(element => (
-                                        <option value={element.id}>{element.nombre}</option>
-                                    ))
-                                }
-                            </select>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Cantidad</label>
-                            <InputField type={'text'}></InputField>
-                        </div>
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Precio</label>
-                            <InputField type={'text'}></InputField>
-                        </div>
-                            <div className="flex items-end">
-                                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l">
-                                    Agregar
-                                </button>
-                                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r">
-                                    Eliminar
-                                </button>
-                        </div>
-                    </div>
+                    {
+                        this.state.listaProductosForm.map((_, index) => (
+                            <div key={index} className="grid gap-6 mb-5 md:grid-cols-4">
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Producto</label>
+                                    <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 focus:ring-primary-600 focus:border-primary-600">
+                                        {
+                                            this.productos.map(element => (
+                                                <option key={element.id}>{element.nombre}</option>
+                                            ))
+                                        }
+                                    </select>
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Cantidad</label>
+                                    <InputField type={'text'}></InputField>
+                                </div>
+                                <div>
+                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Precio</label>
+                                    <InputField type={'text'}></InputField>
+                                </div>
+                                <div className="flex items-end">
+                                    <input onClick={() => this.agregarProducto()} type="button" value="Agregar" className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded mx-2 mb-0.5"></input>
+                                    <input onClick={() => this.eliminarProducto(index)} type="button" value="Eliminar" className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded mx-2 mb-0.5"></input>
+                                </div>
+                            </div>
+                        ))
+                    }
                     <div className="mb-5">
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900">Fecha entrega</label>
                         <InputField type={'text'}></InputField>
