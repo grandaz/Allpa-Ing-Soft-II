1c997d21738e37f9b7fef6861e7d913cf963c56f
JanoGomezL <20190857@aloe.ulima.edu.pe>
2023-11-25 18:44:09 -0500

    Update CardPedido.tsx

--- src/components/Cards/CardPedido.tsx
+++ src/components/Cards/CardPedido.tsx
@@ -10,7 +10,6 @@ import { Component } from "react"
 
 
 interface CardPedidoProps {
-  id: number
   profileImage: string
   nombre: string
   fechaCrea: string
@@ -62,7 +61,7 @@ class CardPedido extends Component<CardPedidoProps> {
           </Typography>
         </CardBody>
         <CardFooter className="pt-0">
-          <a href={"/detallePedido/" + this.props.id}><GreenButton type="button" label="Inscribirse"></GreenButton></a>
+          <a href="/detallePedido"><GreenButton type="button" label="Inscribirse"></GreenButton></a>
         </CardFooter>
       </Card>
     )
