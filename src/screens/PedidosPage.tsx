e31cb44b50e9981d055a98fb8e4cd77cc78cbb9d
JanoGomezL <20190857@aloe.ulima.edu.pe>
2023-11-25 20:44:12 -0500

    update

--- src/screens/PedidosPage.tsx
+++ src/screens/PedidosPage.tsx
@@ -86,6 +86,7 @@ class PedidosPage extends Component<PedidosPageProps, PedidosPageState> {
           {this.state.pedidos.map(pedido => (
             <CardPedido
               key={pedido.id}
+              id={0} 
               profileImage={pedido.user?.profileImage ?? DefaultProfile}
               nombre={pedido.user?.firstName + ' ' + pedido.user?.lastName}
               fechaCrea={pedido.createdAt?.substring(0, 10) ?? ''}
