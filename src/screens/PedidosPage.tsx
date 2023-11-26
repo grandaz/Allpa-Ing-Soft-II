8592a932c4c5d1f0704bdc8ec98a37ab1320e31f
PTrabajos <20190895@aloe.ulima.edu.pe>
2023-11-26 06:51:07 -0500

    Piero

--- src/screens/PedidosPage.tsx
+++ src/screens/PedidosPage.tsx
@@ -3,7 +3,7 @@ import { Component } from "react";
 import DefaultProfile from '../assets/default_profile.jpg';
 import SearchBar from "../components/Searchs/SearchBar";
 import DropdownComponent from "../components/Searchs/DropdownComponent";
-
+import GreenButton from "../components/Inputs/GreenButton";
 import OrderTO from "../to/OrderTO"
 import OrderManager from "../manager/OrderManager"
 
